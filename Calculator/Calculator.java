916297adf9826b0c3a7ebb10cf95cda02deba891
Shashank Alagawadi <01fe22bca144@kletech.ac.in>
2024-01-24 16:13:35 +0530

    Updated the code changes related to Square cube and modulo operations

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -2,9 +2,11 @@
  * The Calculator class performs basic arithmetic operations such as addition, subtraction,
  * multiplication, and division.
  */
+
 package Calculator;
 
-class Calculator{
+public class Calculator extends Operations{
+
   // The code you provided is defining four methods in the Calculator class: addition, subtraction,
   // multiplication, and division.
     float addition(float opr1,float opr2){
@@ -25,7 +27,9 @@ class Calculator{
  * multiplication, and division operations on two operands, and prints the results.
  */
     public static void main(String[] args) {
+        // float square,cube,modulus;
         Calculator calc= new Calculator();
+        // Operations calc1=new Operations();
         float opr1 =10;
         float opr2 =63;
         float add=calc.addition(opr1, opr2);
@@ -37,6 +41,12 @@ class Calculator{
         float div=calc.division(opr1, opr2);
         System.out.println("Division = "+ div);
 
+        float sq=calc.square(opr1);        // Call square method
+        System.out.println("Square = " + sq);  // Print square result
+        float cu=calc.cube(opr1);          // Call cube method
+        System.out.println("Cube = " + cu);    // Print cube result
+        float mu=calc.modulus(opr1, opr2);  // Call modulus method
+        System.out.println("Modulus = " + mu);  
 
     }
 }
